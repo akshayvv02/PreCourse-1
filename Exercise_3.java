757fbba0b51ccc2554c53b7503a21ce56659aaca
Akshay Vijayvergiya <akvv2596@gmail.com>
2025-05-08 18:37:44 -0400

    Exercise_3.java code completed

--- Exercise_3.java
+++ Exercise_3.java
@@ -2,7 +2,12 @@ import java.io.*;
   
 // Java program to implement 
 // a Singly Linked List 
+
 public class LinkedList { 
+    // Time Complexity: O(n) -> insert (function since inserting at end), printList (iterating till the end of the list)
+    // Space Complexity: O(n)  -> (looks similar to array but linkedlist takes extra space per node for reference of next objects)
+    //Did this code successfully run on Leetcode : Yes (works on Compilers)
+    // Any problem you faced while coding this : Not as such
   
     Node head; // head of list 
   
@@ -17,7 +22,8 @@ public class LinkedList {
         // Constructor 
         Node(int d) 
         { 
-            //Write your code here 
+            //assinging the data to the node
+            this.data = d;
         } 
     } 
   
@@ -25,26 +31,43 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+        Node node = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if(list.head == null)
+            list.head = node;
             // Else traverse till the last node 
             // and insert the new_node there 
-
+        else
+        {
+            Node ref = list.head;
+            //checking till the node is null i.e. the last element
+            while(ref.next != null){
+                ref = ref.next;
+            }
             // Insert the new_node at last node 
+            ref.next = node;
+        }
         // Return the list by head 
-        
+        return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        //creating a node to iterate and pointing it to root
+        Node node = list.head;
+        //checking till the node is null i.e. the last element
+        while(node!=null){
             // Print the data at current node 
-       
+            System.out.print(node.data);
             // Go to next node 
+            node = node.next;
+            // adjusting the comma for better visibility
+            if(node != null) System.out.print(", ");
+        }
+         System.out.println("");
     } 
    
     // Driver code 
